82dce2610eb70befbaabe5f555bd38e787914cbb
lesantosx <leticia.santos1997@outlook.com>
2025-01-24 20:12:01 -0300

    feat: add integration with api

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { RouterProvider } from 'react-router'
 import { HelmetProvider, Helmet } from 'react-helmet-async'
+import { QueryClientProvider } from '@tanstack/react-query'
 import { Toaster } from 'sonner'
 
 import './global.css'
 
 import { router } from './routes'
 import { ThemeProvider } from './components/theme/theme-provider'
+import { queryClient } from './lib/react-query'
 
 export function App() {
 
@@ -14,8 +16,11 @@ export function App() {
       <ThemeProvider storageKey='pizzashop-theme' defaultTheme='dark'>
         <Helmet titleTemplate="%s | pizza.shop" />
         <Toaster richColors />
-        <RouterProvider router={router} />
-      </ThemeProvider>      
+
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>       
+      </ThemeProvider>
     </HelmetProvider>    
   )
 }
