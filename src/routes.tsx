deb8ffbf348138f5f3af535f046c937cad7a0937
lesantosx <leticia.santos1997@outlook.com>
2025-01-23 22:02:52 -0300

    feat: Create orders page, table list and pagination

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,16 +1,20 @@
 import { createBrowserRouter } from 'react-router'
+
 import { AppLayout } from './pages/layouts/app'
 import { AuthLayout } from './pages/layouts/auth'
 import { Dashboard } from './pages/app/dashboard'
+import { Orders } from './pages/app/Orders'
 import { SignIn } from './pages/auth/sign-in'
 import { SignUp } from './pages/auth/sign-up'
+
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
     children: [
-      { path: '/', element: <Dashboard /> }
+      { path: '/', element: <Dashboard /> },
+      { path: '/orders', element: <Orders /> },
     ]
   },
   {
